37b1fea8a43832dcdb77be78ebc848cd20905264
Ivan Vieira <iivnn00@gmail.com>
2020-08-28 19:18:03 -0300

    driver_change

--- src/connection/factory/ConnectionFactory.java
+++ src/connection/factory/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/venda";
     private static final String USER = "root";
     private static final String PASSWORD = "";
